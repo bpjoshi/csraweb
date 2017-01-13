9fa48815d42ff04b166e91f7b268a233b92945dc
bpjoshi <bpjoshi@bpjoshi.com>
2017-01-13 16:56:00 +0530

    Final Js changes

--- mod4-solution/SpeakGoodBye.js
+++ mod4-solution/SpeakGoodBye.js
@@ -1,7 +1,8 @@
-(function(){
+(function(window){
   byeSpeaker={};
   var speakWord = "Good Bye";
   byeSpeaker.speak=function speak(name) {
     console.log(speakWord + " " + name);
   }
-})();
+  window.byeSpeaker=byeSpeaker;
+})(window);
--- mod4-solution/SpeakHello.js
+++ mod4-solution/SpeakHello.js
@@ -1,8 +1,8 @@
-(function(){
+(function(window){
   helloSpeaker={};
   var speakWord = "Hello";
   helloSpeaker.speak=function speak(name) {
     console.log(speakWord + " " + name);
   }
-
-})();
+  window.helloSpeaker=helloSpeaker;
+})(window);
